692e1c0604efbd19893a10a3806f8ea9744838a3
Ars <sosyanarsen@gmail.com>
2025-03-19 21:17:50 +0500

    проект с ООП, JUnit, maven

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Calc calc = new Calc(1, 0);
+        Calc calc = new Calc(-1, 0);
         System.out.println(calc.sum());
         System.out.println(calc.subtract());
         System.out.println(calc.multiply());
